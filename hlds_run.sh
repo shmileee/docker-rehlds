19a5443b5751fc854f4f61d407fd2cfff7c7ef40
Aleksandr Ponomarov <ponomarov.aleksandr@gmail.com>
2020-04-25 02:09:23 +0200

    added docker-compose

--- hlds_run.sh
+++ hlds_run.sh
@@ -2,16 +2,6 @@
 
 set -axe
 
-CONFIG_FILE="/opt/hlds/startup.cfg"
-
-if [ -r "${CONFIG_FILE}" ]; then
-    # TODO: make config save/restore mechanism more solid
-    set +e
-    # shellcheck source=/dev/null
-    source "${CONFIG_FILE}"
-    set -e
-fi
-
 EXTRA_OPTIONS=( "$@" )
 
 EXECUTABLE="/opt/hlds/hlds_run"
@@ -29,7 +19,5 @@ fi
 if [ -n "${ADMIN_STEAM}" ]; then
     echo "\"STEAM_${ADMIN_STEAM}\" \"\"  \"abcdefghijklmnopqrstu\" \"ce\"" >> "/opt/hlds/cstrike/addons/amxmodx/configs/users.ini"
 fi
-
-set > "${CONFIG_FILE}"
 
 exec "${EXECUTABLE}" "${OPTIONS[@]}" "${EXTRA_OPTIONS[@]}"
